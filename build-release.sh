dce673a2fe395ea7702293fd1792006207f59aba
thautwarm <twshere@outlook.com>
2020-11-27 22:34:31 +0900

    tagged release

--- build-release.sh
+++ build-release.sh
@@ -9,5 +9,6 @@ fi
 
 cp `stack path --dist-dir`/build/fff-lex/fff-lex hsbin-dist/
 cp `stack path --dist-dir`/build/fff-pgen/fff-pgen hsbin-dist/
+export RELEASE_TAG="`$PY version.py`"
 export ZIP_FILE="fff-`$PY release-env.py plat`-`$PY version.py`.zip"
 zip -r $ZIP_FILE hsbin-dist/*
